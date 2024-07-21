52f6aa39c032767f343a818cde240550b174a16e
sbx_user1051 <sbx_user1051@169.254.31.165>
2024-07-21 13:27:06 +0000

    Sun Jul 21 13:27:06 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,6 +7,8 @@ def checkout(skus):
         if sku not in basket:
             return -1
 
+    total_checkout = 0
+
     return -1
 
 
@@ -29,5 +31,12 @@ basket = {
     'C': Item('C', 20),
     'D': Item('D', 15),
 }
+
+def get_item_checkout_price(item: Item, quantity: int):
+    if item.special_offer:
+        return item.special_offer.price
+
+    return item.price
+
 
 
