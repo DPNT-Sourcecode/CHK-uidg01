35beb57915ae18ef9aae34ab41e093c498932578
sbx_user1051 <sbx_user1051@169.254.31.165>
2024-07-21 18:22:00 +0000

    Sun Jul 21 18:22:00 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -15,6 +15,7 @@ def checkout(skus):
     if any_offer := any_three_items_offer(skus_counter):
         total_checkout += any_offer
 
+    print(skus_counter)
     free_items = get_free_items(skus_counter)
 
     for free_item in free_items:
@@ -91,6 +92,7 @@ def any_three_items_offer(counter):
 
     ordered_by_price = order_items_by_price(counter)
     ordered_by_price = [sku for sku in ordered_by_price if sku in items]
+    print(ordered_by_price)
 
     if counted >= 3:
         sum_skus = sum([counter[item] for item in items])
@@ -104,8 +106,10 @@ def any_three_items_offer(counter):
                 if quantity - diff > 0:
                     counter[sku] -= diff
                 else:
+                    print(f"sku: {sku}, quantity: {quantity}, diff: {diff}")
                     del counter[sku]
                     diff -= quantity
+    print(offer)
     return offer
 
 def order_items_by_price(counter):
@@ -142,6 +146,7 @@ store = {
     'Y': Item('Y', 20), # buy any 3 of (S,T,X,Y,Z) for 45
     'Z': Item('Z', 21), # buy any 3 of (S,T,X,Y,Z) for 45
 }
+
 
 
 
