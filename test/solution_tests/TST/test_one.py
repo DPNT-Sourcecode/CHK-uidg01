dd6963b5d0af8c8d4588ac2bcef2b13407b84fb2
sbx_user1051 <sbx_user1051@169.254.31.165>
2024-07-21 15:00:28 +0000

    Sun Jul 21 15:00:28 UTC 2024

--- test/solution_tests/TST/test_one.py
+++ test/solution_tests/TST/test_one.py
@@ -1,6 +1,24 @@
 from solutions.TST import one
+from solutions.CHK import checkout
 
 
 class TestSum():
     def test_sum(self):
         assert one.get() == 1
+
+class TestCheckut():
+    def test_checkout(self):
+        assert checkout('A') == 50
+        assert checkout('B') == 30
+        assert checkout('C') == 20
+        assert checkout('D') == 15
+        assert checkout('AA') == 100
+        assert checkout('BB') == 45
+        assert checkout('AAA') == 130
+        assert checkout('BBB') == 75
+        assert checkout('AAAA') == 180
+        assert checkout('ABCD') == 115
+        assert checkout('AAAB') == 160
+        assert checkout('AAABB') == 175
+        assert checkout('AAABBD') == 190
+
