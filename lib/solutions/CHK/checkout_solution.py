7d1b8db82cc61b5910e75d20392cb57735414496
sbx_user1051 <sbx_user1051@169.254.31.165>
2024-07-21 13:24:06 +0000

    Sun Jul 21 13:24:06 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,7 +1,33 @@
-
+from dataclasses import dataclass
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    for sku in skus:
+        if sku not in basket:
+            return -1
+
     return -1
+
+
+@dataclass
+class SpecialOffer:
+    quantity: int
+    price: int
+
+
+@dataclass
+class Item:
+    name: str
+    price: int
+    special_offer: SpecialOffer | None = None
+
+
+basket = {
+    'A': Item('A', 50, SpecialOffer(3, 130)),
+    'B': Item('B', 30, SpecialOffer(2, 45)),
+    'C': Item('C', 20),
+    'D': Item('D', 15),
+}
+
 
